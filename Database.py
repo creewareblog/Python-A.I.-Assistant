69eb687dc5d53a1c5d80788dcb2d5920bab50680
Ishwar Jagdale <49467965+creewareblog@users.noreply.github.com>
2019-06-30 00:54:00 +0530

    Add files via upload

---
+++ Database.py
@@ -0,0 +1,5 @@
+#You can add other words greet as you want
+greet = ('Hello', 'Hi', 'Hey Alexa', 'Hey', 'Good to see you', 'Nice to see you')
+
+#These are sample names and their corresponding email, change them with your recipient as 'Name': 'Email'
+maildict = { 'John': 'john@gmail.com', 'Sam': 'sam@gmail.com'}
---
+++ alexa.py
@@ -0,0 +1,113 @@
+from gtts import gTTS
+import speech_recognition as sr
+import os
+from time import ctime
+import webbrowser
+import smtplib
+import wikipedia
+import Database
+
+def speak(audio):
+	tts = gTTS(text=audio, lang='en')
+	tts.save('audio.mp3')
+	os.system('audio.mp3')
+	
+def reply(message):
+	print(message)
+	speak(message)
+
+def mic():
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		reply("How can I help you?\nListening...")
+		r.pause.threshold = 1
+		r.adjust_for_ambient_noise(source)
+		audio = r.listen(source)
+	
+	try:
+		command = r.recognize_google(audio)
+		print("Recognizing: ", command)
+	except sr.UnknownValueError:
+		reply("Google Speech Recognition couldn't understand, please try again.")
+	except sr.RequestError as e:
+		print("Couldn't request results;{0}".format(e))
+	return command
+	
+def bot(command):
+	for x in greet:
+		if x == command:
+			reply('Hello Sir')
+	
+	for x in info:
+		if x == command:
+			reply("I am " + botname + ", a virtual assistant program, made in python language, for you")
+			
+	if "What time is it" or "Tell me the time" in command:
+		speak(ctime())
+		
+	if "Where is" in command:
+		command = command.split(" ")
+		location = command[2]
+		reply("Hold on, I will check.")
+		chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+		webbrowser.get(chrome_path).open("https://www.google.nl/maps/place/" + location + "/&amp;")
+
+	if 'email' in command:
+		reply('Who is the recipient?')
+		recipient = mic()
+		if x in recipient:
+			if x in maildict:
+				reply('What should I say?')
+				content = mic()
+				mail = smtplib.SMTP('smtp.gmail.com', 587)
+				mail.ehlo()
+				mail.starttls()
+				mail.login('Your Email', 'Your Password')
+				mail.sendmail(x, maildict[x], content)
+				mail.close()
+				reply('Email sent.')
+            
+		else:
+			reply('I don\'t know what you mean!')
+
+	if "wikipedia" in command:
+		command = command.replace("wikipedia", " ")
+		information = wikipedia.summary(command, sentences = 2)
+		reply("Wikipedia says...\n" information)
+	
+	if "open youtube" in command:
+		webbrowser.open("https://youtube.com")
+		speak("Opening")
+
+	if "open my channel" or "open my youtube channel" in command:
+		webbrowser.open("https://www.youtube.com/channel/UCGTIYo3TPAEHVFbY2uU22ig")
+		speak("Opening")
+
+	if "open my blog" or "open my website" in command:
+		webbrowser.open("https://www.creeware.blogspot.com/")
+		speak("Opening")
+	
+	if "open google" in command:
+		webbrowser.open("https://google.com")
+		speak("Opening")
+
+	if 'play songs' in command:
+		music_dir = 'D:\\Songs'
+		songs = os.listdir(music_dir)
+		os.startfile(os.path.join(music_dir, songs[0]))
+
+
+reply('Hi, I am Alexa, a virtual assistant program, your desktop companion.\nDo you want to give me a name?')
+quest = input('Type Yes or No: ')
+if quest == 'Yes':
+	speak('Enter a Name for me: ')
+	botname = input('Enter a Name for me: ')
+	reply("Nice name, I will remember it")
+else:
+	botname = "Alexa"
+	reply('Ok, then, we will go with Alexa')
+
+
+while True:
+	command = mic()
+	
